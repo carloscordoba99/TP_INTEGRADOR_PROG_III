7d6e544def4a66226b03cc7126035463f5ddeece
FranC <73913524+FranciscoCrestanello@users.noreply.github.com>
2021-11-20 08:15:05 -0300

    Carrito de compras

---
+++ Dao/DaoCarritos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dao
+{
+    public class DaoCarritos
+    {
+        AccesoDatos dt = new AccesoDatos();
+        public Boolean ExisteCarrito(Carrito cat)
+        {
+            String consulta = "Select * from Categorias where CodCategoria='" + cat.getCodCategoria() + "'";
+            return dt.existe(consulta);
+        }
+    }
+}
---
+++ Entidades/Carritos.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Carritos
+    {
+        private int CodCarrito;
+        private int CodUsuario;
+        private int CodArticulo;
+        private string Descripcion;
+        private decimal Precio;
+        private int Cantidad;
+        private decimal SubTotal;
+        private bool Comprado;
+
+        public Carritos() { }
+        public Carritos(int CodUsuario_, int CodArticulo_, string Descripcion_, decimal Precio_, int Cantidad_, decimal SubTotal_, bool Comprado_)
+        {
+            this.CodUsuario = CodUsuario_;
+            this.CodArticulo = CodArticulo_;
+            this.Descripcion = Descripcion_;
+            this.Precio = Precio_;
+            this.Cantidad = Cantidad_;
+            this.SubTotal = SubTotal_;
+            this.Comprado = Comprado_;
+        }
+
+        public void SetCodUsuario(int x) { this.CodUsuario = x; }
+        public int GetCodUsuario() { return CodUsuario; }
+        public void SetCodArticulo(int x) { this.CodArticulo = x; }
+        public int GetCodArticulo() { return CodArticulo; }
+        public void SetDescripcion(string x) { this.Descripcion = x; }
+        public string GetDescripcion() { return Descripcion; }
+        public void SetPrecio(Decimal x) { this.Precio = x; }
+        public decimal GetPrecio() { return Precio; }
+        public void SetCantidad(int x) { this.Cantidad = x; }
+        public int GetCantidad() { return Cantidad; }
+        public void SetSubTotal(decimal x) { this.SubTotal = x; }
+        public decimal GetSubTotal() { return SubTotal; }
+        public void SetComprado(bool x) { this.Comprado = x; }
+        public bool GetComprado() { return Comprado; }
+
+    }
+}
